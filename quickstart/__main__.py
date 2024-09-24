b5671dd7ba517f3fb9ff99f4e2bbd49e0aa98075
Jonatan <jtuv@kth.se>
2024-09-24 09:10:53 +0200

    Start spucing up code

--- quickstart/__main__.py
+++ quickstart/__main__.py
@@ -1,10 +1,10 @@
-"""A Google Cloud Python Pulumi program"""
+"""Fast and Loose Inc: Resource management"""
 
 import pulumi
 from pulumi_gcp import storage
 
 # Create a GCP resource (Storage Bucket)
-bucket = storage.Bucket('my-bucket', location="US")
+userData = storage.Bucket('my-bucket', location="US")
 
 binding = storage.BucketIAMBinding ("member", 
  bucket= bucket.name,
